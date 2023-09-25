ed2c4f83e589dd10b72080c93eb7a3bf29d50bf6
Android <gguptag18@gmail.com>
2023-09-25 13:37:07 +0530

    hash

--- pattern/rough.java
+++ pattern/rough.java
@@ -4,14 +4,12 @@ public class rough {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int x = scn.nextInt();
-        int digit = 0;
-        int mult = (int) Math.pow(10, (int) Math.log10(x));
-        while (x != 0) {
-            digit = digit + mult * (x % 10); // 320
-            x = x / 10; // 1
-            mult = mult / 10; // 1
-        }
-        System.out.println(digit);
+        String[] arr = { "a", "b", "ba", "bca", "bda", "bdca" };
+
+        Arrays.sort(arr, Comparator.comparing(String::length)); // sort ther string array with respect to length of the
+                                                                // string element
+
+        Arrays.sort(arr, (a, b) -> a.length() - b.length()); // sorting wrt to length
+        System.out.println(Arrays.toString(arr));
     }
 }
